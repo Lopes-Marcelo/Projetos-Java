963214442147f6b9a75d29ec8b89bc1048864fbe
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-20 20:52:21 -0400

    Inicialização de projeto básico com spring boot

---
+++ projeto-basico-springboot/src/main/java/projetobasico/projetobasicospringboot/ProjetoBasicoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package projetobasico.projetobasicospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBasicoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBasicoSpringbootApplication.class, args);
+	}
+
+}
---
+++ projeto-basico-springboot/src/test/java/projetobasico/projetobasicospringboot/ProjetoBasicoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package projetobasico.projetobasicospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBasicoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
