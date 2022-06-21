d93afb8f5bb1d16bfbc18f18b66ca1a28f939895
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-20 22:32:33 -0400

    Projeto Básico com Spring Boot - Curso: Introdução ao Framework Spring Boot

---
+++ projeto-curso-dio-springboot/src/main/java/com/projetocursodio/projetocursodiospringboot/ProjetoCursoDioSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.projetocursodio.projetocursodiospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoDioSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoDioSpringbootApplication.class, args);
+	}
+
+}
---
+++ projeto-curso-dio-springboot/src/test/java/com/projetocursodio/projetocursodiospringboot/ProjetoCursoDioSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocursodio.projetocursodiospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoDioSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
