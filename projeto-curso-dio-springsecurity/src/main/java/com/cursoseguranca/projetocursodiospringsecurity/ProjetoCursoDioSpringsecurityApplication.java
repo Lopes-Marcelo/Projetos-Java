e9b153c0913f456bce60f33e83955e5314e2916a
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-21 19:31:03 -0400

    Projeto Básico com Spring Boot - Curso: Adcionando Segurança a uma API Rest com Spring Security

---
+++ projeto-curso-dio-springsecurity/src/main/java/com/cursoseguranca/projetocursodiospringsecurity/ProjetoCursoDioSpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cursoseguranca.projetocursodiospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoDioSpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoDioSpringsecurityApplication.class, args);
+	}
+
+}
---
+++ projeto-curso-dio-springsecurity/src/test/java/com/cursoseguranca/projetocursodiospringsecurity/ProjetoCursoDioSpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoseguranca.projetocursodiospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoDioSpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
